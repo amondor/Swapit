4da9798faa86b82601a53e0e9988c7b7863d9813
Andrew Mondor <mondor.andrew@gmail.com>
2020-12-27 00:23:13 +0100

    ProfilPage integration

---
+++ app/src/Controller/ProfilPageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfilPageController extends AbstractController
+{
+    /**
+     * @Route("/profil/page", name="profil_page")
+     */
+    public function index()
+    {
+        return $this->render('profil_page/index.html.twig', [
+            'controller_name' => 'ProfilPageController',
+        ]);
+    }
+}
